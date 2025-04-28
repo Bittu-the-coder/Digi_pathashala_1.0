a6715bd9b43c8ce08546b4250ffad82db61a7b97
Bittu-the-coder <Bittuprajapati2271@gmail.com>
2025-04-28 15:08:33 +0530

    fixed env problem

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://notevue:one111@cluster0.yfxesp3.mongodb.net/digi?retryWrites=true&w=majority&appName=Cluster0");
+    const conn = await mongoose.connect(process.env.MONGO_URI)
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
